05a1c101f00d095fd27e98374063e69fe08a6785
dresenhista <andresakarine@gmail.com>
2017-10-01 19:56:28 -0400

    rearranging files

---
+++ 6-Functions Example/cachematrix.R
@@ -0,0 +1,35 @@
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+
+
+
+
+# This function assumes that the matrix is always invertible.
+cacheSolve <- function(x, ...) {
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
+}
+
+
+
